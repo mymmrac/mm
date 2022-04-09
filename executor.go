7403c3a16085d2b523c47271be8ca2f299a17a5e
Artem Yadelskyi <mymmrac@gmail.com>
2022-04-09 11:12:28 +0300

    Tokenization

--- executor.go
+++ executor.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 )
 
@@ -32,30 +33,81 @@ func (e *executor) execute(expr string) (string, *exprError) {
 	return strings.Join(mapSlice(tokens, toString[token]), ", "), nil
 }
 
+type tokenKind string
+
+type token struct {
+	kind     tokenKind
+	value    string
+	startPos int
+	endPos   int
+}
+
+func (t token) String() string {
+	return fmt.Sprintf("{%s}:%d-%d `%s`", t.kind, t.startPos, t.endPos, t.value)
+}
+
 type lexer struct{}
 
 func newLexer() *lexer {
 	return &lexer{}
 }
 
-func (l *lexer) tokenize(text string) []token {
-	return nil
-}
-
-type tokenKind string
-
 const (
 	identifier tokenKind = "identifier" // `abc`, `a12`, `a_b_1`
 	number     tokenKind = "number"     // `123`, `1.12`, `-12`, `1_2_3`
-	operator   tokenKind = "operator"   // `+`, `-`, `^`
+	operator   tokenKind = "operator"   // `+`, `-`, `^`, `(`
 )
 
-type token struct {
-	kind  tokenKind
-	value string
-	pos   int
-}
+var (
+	identPattern    = regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9_]*`)
+	numberPattern   = regexp.MustCompile(`^-?[0-9_]+(:?\.[0-9_]+)?`)
+	operatorPattern = regexp.MustCompile(`^[-+*/^()]`)
+)
 
-func (t token) String() string {
-	return fmt.Sprintf("{%s}:%d `%s`", t.kind, t.pos, t.value)
+func (l *lexer) tokenize(text string) []token {
+	var tokens []token
+
+	var (
+		pos     = 0
+		trimmed int
+		loc     []int
+		tKind   tokenKind
+		tValue  string
+	)
+
+	for text != "" {
+		text, trimmed = trimWhitespacesAndCount(text)
+		pos += trimmed
+
+		loc = identPattern.FindStringIndex(text)
+		tKind = identifier
+
+		if len(loc) == 0 {
+			loc = numberPattern.FindStringIndex(text)
+			tKind = number
+		}
+
+		if len(loc) == 0 {
+			loc = operatorPattern.FindStringIndex(text)
+			tKind = operator
+		}
+
+		if len(loc) == 0 {
+			panic("TODO: Handle invalid syntax")
+		}
+
+		tValue = text[loc[0]:loc[1]]
+
+		tokens = append(tokens, token{
+			kind:     tKind,
+			value:    tValue,
+			startPos: loc[0] + pos,
+			endPos:   loc[1] + pos,
+		})
+
+		pos += len(tValue)
+		text = text[loc[1]:]
+	}
+
+	return tokens
 }
--- utils.go
+++ utils.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func mapSlice[T1, T2 any](slice []T1, f func(T1) T2) []T2 {
 	newSlice := make([]T2, len(slice))
@@ -12,4 +15,9 @@ func mapSlice[T1, T2 any](slice []T1, f func(T1) T2) []T2 {
 
 func toString[T fmt.Stringer](a T) string {
 	return a.String()
+}
+
+func trimWhitespacesAndCount(text string) (string, int) {
+	newText := strings.TrimLeft(text, " \t")
+	return newText, len(text) - len(newText)
 }
